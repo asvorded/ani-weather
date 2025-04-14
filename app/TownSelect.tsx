cdc12be6cb5834f6662142642488e3a03a716c09
Artur Levitskij <levitskijartur@gmail.com>
2025-04-14 14:48:25 +0300

    changed formatting

--- app/TownSelect.tsx
+++ app/TownSelect.tsx
@@ -1,31 +1,31 @@
-import { Image, StyleSheet, Text, TextInput, View } from "react-native";
+import { Image, StyleSheet, Text, TextInput, View } from 'react-native';
 
-import { useTranslation } from "react-i18next";
-
+import { useTranslation } from 'react-i18next';
+import React  from 'react';
 const TownSelect = () => {
-  let { t, i18n } = useTranslation()
+  let { t } = useTranslation();
 
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.input}
-          placeholder={t("townSelect.textField.placeholder")}
+          placeholder={t('townSelect.textField.placeholder')}
           numberOfLines={1}
           />
         <Image
           style={styles.locationIcon}
-          source={require("./../assets/logos/location.png")}
+          source={require('./../assets/logos/location.png')}
           />
       </View>
-      <Text>{t("townSelect.popularCities")}</Text>
+      <Text>{t('townSelect.popularCities')}</Text>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   screen: {
-    paddingHorizontal: 10
+    paddingHorizontal: 10,
   },
   inputContainer: {
     flexDirection: 'row',
@@ -34,19 +34,19 @@ const styles = StyleSheet.create({
 
     paddingHorizontal: 10,
 
-    backgroundColor: "#f4f4f4",
-    borderColor: "#b7b7b7",
+    backgroundColor: '#f4f4f4',
+    borderColor: '#b7b7b7',
     borderWidth: 1,
-    borderRadius: 150
+    borderRadius: 150,
   },
   input: {
     flex: 1,
-    fontSize: 16
+    fontSize: 16,
   },
   locationIcon: {
     width: 28,
-    height: 28
-  }
-})
+    height: 28,
+  },
+});
 
 export default TownSelect;
