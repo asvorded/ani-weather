b580df41e5834e40cbb6448ae148e7ef8eff8995
Artur Levitskij <levitskijartur@gmail.com>
2025-04-15 22:25:14 +0300

    added font

--- app/App.tsx
+++ app/App.tsx
@@ -9,8 +9,13 @@ import React from 'react';
 import { SystemBars } from 'react-native-edge-to-edge';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import TownSelect from './TownSelect';
+import {useFonts} from 'expo-font';
 
 const App = () => {
+    useFonts({
+        'BellotaText-Regular': require('./../assets/fonts/BellotaText-Regular.ttf'),
+    });
+
   return (
     <SafeAreaProvider>
       <SystemBars style="auto"/>
