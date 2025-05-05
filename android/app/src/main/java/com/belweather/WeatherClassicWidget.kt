d541c363802a9b24089a2e4e92103210abc76ed9
Maksim Dadush <pud700max@gmail.com>
2025-05-05 16:46:20 +0300

    Added clock, calendar and app intents to widget

--- android/app/src/main/java/com/belweather/WeatherClassicWidget.kt
+++ android/app/src/main/java/com/belweather/WeatherClassicWidget.kt
@@ -1,8 +1,12 @@
 package com.belweather
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.provider.AlarmClock
 import android.widget.RemoteViews
 import java.text.SimpleDateFormat
 import java.util.Date
@@ -17,32 +21,44 @@ class WeatherClassicWidget : AppWidgetProvider() {
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
-        for (appWidgetId in appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId)
+        appWidgetIds.forEach { appWidgetId ->
+            val weatherPendingIntent = PendingIntent.getActivity(
+                context, 0,
+                Intent(context, MainActivity::class.java),
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+            val timePendingIntent = PendingIntent.getActivity(
+                context, 1,
+                Intent(AlarmClock.ACTION_SHOW_ALARMS),
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+            val calendarPendingIntent = PendingIntent.getActivity(
+                context, 2,
+                Intent(Intent.ACTION_VIEW).apply {
+                    data = Uri.parse("content://com.android.calendar/time")
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                },
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+
+            val views = RemoteViews(context.packageName, R.layout.weather_classic_widget).apply {
+                    setOnClickPendingIntent(R.id.weather_view, weatherPendingIntent)
+                    setOnClickPendingIntent(R.id.time_text, timePendingIntent)
+                    setOnClickPendingIntent(R.id.date_text, calendarPendingIntent)
+
+                    setWidgetDateAndTime()
+                }
+
+            appWidgetManager.updateAppWidget(appWidgetId, views)
         }
-    }
-
-    override fun onEnabled(context: Context) {
-
-    }
-
-    override fun onDisabled(context: Context) {
-
     }
 }
 
-internal fun updateAppWidget(
-    context: Context,
-    appWidgetManager: AppWidgetManager,
-    appWidgetId: Int
-) {
-    // Get current time
-    val widgetText = SimpleDateFormat("HH:mm", Locale.getDefault()).format(Date())
+internal fun RemoteViews.setWidgetDateAndTime() {
+    val currentDate = Date()
+    val time = SimpleDateFormat("HH:mm", Locale.getDefault()).format(currentDate)
+    val date = SimpleDateFormat("EE, d MMMM", Locale.getDefault()).format(currentDate)
 
-    // Construct the RemoteViews object
-    val views = RemoteViews(context.packageName, R.layout.weather_classic_widget)
-    views.setTextViewText(R.id.time_text, widgetText)
-
-    // Instruct the widget manager to update the widget
-    appWidgetManager.updateAppWidget(appWidgetId, views)
+    this.setTextViewText(R.id.time_text, time)
+    this.setTextViewText(R.id.date_text, date)
 }
