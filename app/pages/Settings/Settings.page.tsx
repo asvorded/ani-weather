f2847c0054e8a6a13f47a2b838bfead7134f8c1f
Artur Levitskij <levitskijartur@gmail.com>
2025-05-09 01:07:01 +0300

    redesigned settings page

--- app/pages/Settings/Settings.page.tsx
+++ app/pages/Settings/Settings.page.tsx
@@ -1,8 +1,11 @@
-import React, {useCallback, useEffect, useState} from 'react';
-import {View, Text, StyleSheet, Switch} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
+import React from 'react';
+import {
+  View,
+  Switch,
+  ImageBackground,
+  ScrollView,
+} from 'react-native';
 import {useTranslation} from 'react-i18next';
-import {useCustomNavigation} from '../../hooks/useCustomNavigation.ts';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {SystemBars} from 'react-native-edge-to-edge';
 import {Picker} from '@react-native-picker/picker';
@@ -12,14 +15,27 @@ import {
   WindSpeedUnits,
 } from '../../types/api/Forecast.ts';
 import {Languages} from '../../types/storage/Languages.ts';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import {UserSettings} from '../../types/storage/UserSettings.ts';
 import {useUserSettings} from '../../services/UserSettingsProvider.tsx';
+import {CustomText} from '../../components/CustomText/CustomText.tsx';
+import {styles} from './Settings.styles.ts';
 
+type SettingsPanelProps = {
+  color: string;
+  title: string;
+  contentElement: React.ReactNode;
+};
+
+const SettingsPanel = ({color, title, contentElement}: SettingsPanelProps) => {
+  return (
+    <View style={[styles.settingsPanel, {backgroundColor: color + 'C0'}]}>
+      <CustomText style={styles.settingLabel}>{title}</CustomText>
+      {contentElement}
+    </View>
+  );
+};
 const SettingsPage = () => {
   const {userSettings, setUserSettings} = useUserSettings();
-  const {t, i18n} = useTranslation();
-  const navigation = useCustomNavigation();
+  const {t} = useTranslation();
   const insets = useSafeAreaInsets();
 
   function setLanguage(value: Languages) {
@@ -48,122 +64,139 @@ const SettingsPage = () => {
   }
 
   return (
-    <View style={styles.container}>
-      <View
-        style={{
+    <ImageBackground
+      style={styles.background}
+      source={require('../../../assets/images/sample.png')}>
+      <ScrollView
+        contentContainerStyle={{
           marginTop: insets.top,
           marginBottom: insets.bottom,
           marginLeft: insets.left,
           marginRight: insets.right,
-        }}>
+          padding: 16,
+        }}
+        showsVerticalScrollIndicator={false}>
         <SystemBars style="light" />
-        <Text style={styles.header}>{t('settings.title')}</Text>
+        <CustomText style={styles.header}>{t('settings.title')}</CustomText>
 
-        <View style={styles.settingItem}>
-          <Text style={styles.settingLabel}>{t('settings.language')}</Text>
-          <Picker
-            selectedValue={userSettings.language}
-            mode="dropdown"
-            style={styles.picker}
-            onValueChange={value => setLanguage(value)}>
-            <Picker.Item label="English" value={Languages.English} />
-            <Picker.Item label="Русский" value={Languages.Russian} />
-          </Picker>
-        </View>
+        <SettingsPanel
+          color="#A9E788"
+          title={t('settings.language')}
+          contentElement={
+            <Picker
+              selectedValue={userSettings.language}
+              mode="dropdown"
+              style={styles.picker}
+              itemStyle={[styles.pickerItem]}
+              onValueChange={value => setLanguage(value)}>
+              <Picker.Item
+                style={styles.text}
+                label="English"
+                value={Languages.English}
+              />
+              <Picker.Item
+                style={styles.text}
+                label="Русский"
+                value={Languages.Russian}
+              />
+            </Picker>
+          }
+        />
 
-        {/* Measurement Units */}
-        <View style={styles.settingItem}>
-          <Text style={styles.settingLabel}>{t('settings.units.title')}</Text>
-        </View>
-        <View style={styles.settingItem}>
-          <Text style={styles.settingLabel}>
-            {t('settings.units.temperature')}
-          </Text>
-          <Picker
-            selectedValue={userSettings.temperature}
-            style={styles.picker}
-            mode="dropdown"
-            itemStyle={{color: 'white'}}
-            onValueChange={value => setTemperatureUnits(value)}>
-            <Picker.Item label="Celsius" value={TempUnits.Celsius} />
-            <Picker.Item label="Fahrenheit" value={TempUnits.Fahrenheit} />
-            <Picker.Item label="Kelvin" value={TempUnits.Kelvin} />
-          </Picker>
-        </View>
-        <View style={styles.settingItem}>
-          <Text style={styles.settingLabel}>
-            {t('settings.units.pressure')}
-          </Text>
-          <Picker
-            selectedValue={userSettings.pressure}
-            style={styles.picker}
-            mode="dropdown"
-            itemStyle={{color: 'white'}}
-            onValueChange={value => setPressureUnits(value)}>
-            <Picker.Item label="Pascals" value={PressureUnits.Pascal} />
-            <Picker.Item
-              label="Millimeters of Mercury"
-              value={PressureUnits.MmHg}
+        <SettingsPanel
+          color="#B3DBFF"
+          title={t('settings.units.temperature')}
+          contentElement={
+            <Picker
+              selectedValue={userSettings.temperature}
+              style={styles.picker}
+              mode="dropdown"
+              itemStyle={[styles.pickerItem]}
+              onValueChange={value => setTemperatureUnits(value)}>
+              <Picker.Item
+                style={styles.text}
+                label="Celsius"
+                value={TempUnits.Celsius}
+              />
+              <Picker.Item
+                style={styles.text}
+                label="Fahrenheit"
+                value={TempUnits.Fahrenheit}
+              />
+              <Picker.Item
+                style={styles.text}
+                label="Kelvin"
+                value={TempUnits.Kelvin}
+              />
+            </Picker>
+          }
+        />
+
+        <SettingsPanel
+          color="#FFE179"
+          title={t('settings.units.pressure')}
+          contentElement={
+            <Picker
+              selectedValue={userSettings.pressure}
+              style={[styles.text, styles.picker]}
+              mode="dropdown"
+              itemStyle={[styles.pickerItem]}
+              onValueChange={value => setPressureUnits(value)}>
+              <Picker.Item
+                style={styles.text}
+                label="Pascals"
+                value={PressureUnits.Pascal}
+              />
+              <Picker.Item
+                style={styles.text}
+                label="Millimeters of Mercury"
+                value={PressureUnits.MmHg}
+              />
+            </Picker>
+          }
+        />
+
+        <SettingsPanel
+          color="#FBB9BA"
+          title={t('settings.units.wind')}
+          contentElement={
+            <Picker
+              selectedValue={userSettings.windSpeed}
+              style={styles.picker}
+              mode="dropdown"
+              itemStyle={[styles.pickerItem]}
+              onValueChange={value => setWindSpeedUnits(value)}>
+              <Picker.Item
+                style={styles.text}
+                label="M/s"
+                value={WindSpeedUnits.Ms}
+              />
+              <Picker.Item
+                label="Km/s"
+                style={styles.text}
+                value={WindSpeedUnits.Kmh}
+              />
+            </Picker>
+          }
+        />
+
+        <SettingsPanel
+          color="#FF9A79"
+          title={t('settings.notifications')}
+          contentElement={
+            <Switch
+              trackColor={{false: '#767577', true: '#81b0ff'}}
+              thumbColor={userSettings.notifications ? '#f5dd4b' : '#f4f3f4'}
+              ios_backgroundColor="#3e3e3e"
+              value={userSettings.notifications}
+              onValueChange={value => setNotifications(value)}
             />
-          </Picker>
-        </View>
-        <View style={styles.settingItem}>
-          <Text style={styles.settingLabel}>
-            {t('settings.units.wind')}
-          </Text>
-          <Picker
-            selectedValue={userSettings.windSpeed}
-            style={styles.picker}
-            mode="dropdown"
-            itemStyle={{color: 'white'}}
-            onValueChange={value => setWindSpeedUnits(value)}>
-            <Picker.Item label="M/s" value={WindSpeedUnits.Ms} />
-            <Picker.Item
-              label="Km/s"
-              value={WindSpeedUnits.Kmh}
-            />
-          </Picker>
-        </View>
-
-        {/* Notifications */}
-        <View style={styles.settingItem}>
-          <Text style={styles.settingLabel}>{t('settings.notifications')}</Text>
-          <Switch
-            value={userSettings.notifications}
-            onValueChange={value => setNotifications(value)}
-          />
-        </View>
-      </View>
-    </View>
+          }
+        />
+      </ScrollView>
+    </ImageBackground>
   );
 };
 
 export default SettingsPage;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-    //backgroundColor: '#fff',
-  },
-  header: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  settingItem: {
-    marginBottom: 20,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  settingLabel: {
-    fontSize: 18,
-    marginBottom: 8,
-  },
-  picker: {
-    height: 50,
-    width: '100%',
-    backgroundColor: '#002081',
-  },
-});
---
+++ app/pages/Settings/Settings.styles.ts
@@ -0,0 +1,43 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    height: '100%',
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 20,
+  },
+  settingsPanel: {
+    padding: 16,
+    borderRadius: 20,
+    marginBottom: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  settingLabel: {
+    fontSize: 24,
+    marginBottom: 8,
+  },
+  picker: {
+    height: 50,
+    width: '100%',
+    color: '#fff',
+    backgroundColor: 'rgba(0, 32, 129, 0.6)',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+  },
+  pickerItem: {
+    color: '#a30000',
+    backgroundColor: '#7f0098',
+    fontFamily: 'BellotaText-Regular',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+  },
+  text: {
+    fontFamily: 'BellotaText-Regular',
+  },
+});
